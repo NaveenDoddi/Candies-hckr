7763f034b990df8a4c67fb11ef9ec1b35ae1e845
naveendoddi <prasadnaveen847@gmail.com>
2022-12-26 19:10:19 +0530

    candies hackerrank

---
+++ Candies.js
@@ -0,0 +1,28 @@
+function candies(n, arr) {
+    // Write your code here
+    var sum = 0;
+    var arrr = []
+    var count = 0
+    for(let i=0; i<n; i++){
+        if(arr[i] <= arr[i+1]){
+            count = 1
+            
+        }
+        else{
+            count++ 
+            if (arr[i+1] <+ arr[i+2]){
+               count = 1
+               count++
+            }
+            
+
+        }
+        sum += count
+        arrr[i] = count
+        // console.log(arr[i]<arr[i+1])
+        console.log(arrr)
+        
+        
+    }
+return sum
+}
